ecdd5099046b43938c98892d3a8038ea6b4f445b
Ángel Ilai Alcañiz García <angelilai@MacBook-Pro-de-Angel.local>
2024-05-15 18:50:28 +0200

    perfil

--- how-to-vlc/src/app/pages/perfil/perfil.component.ts
+++ how-to-vlc/src/app/pages/perfil/perfil.component.ts
@@ -1,10 +1,32 @@
-import { Component } from '@angular/core';
+import { LoginComponent } from './../login/login.component';
+import { Component, OnInit } from '@angular/core';
+import { GalleryComponent } from '../eventos/componentes/gallery/gallery.component';
+import { AuthService } from '../../services/auth.service';
 
 @Component({
   selector: 'app-perfil',
   templateUrl: './perfil.component.html',
   styleUrls: ['./perfil.component.css']
 })
-export class PerfilComponent {
 
+export class PerfilComponent {
+  fotosEventos = new GalleryComponent;
+
+  isAuthenticated = false;
+  username = '';
+  nombreUsuario = '';
+  constructor(private authService: AuthService) { }
+
+  ngOnInit(): void {
+    this.isAuthenticated = this.authService.isLoggedIn;
+
+    // Verifica si el usuario está autenticado antes de asignar el nombre de usuario
+    if (this.isAuthenticated) {
+      this.username = this.authService.getUsername();
+      this.nombreUsuario = this.authService.obtenerNombreUsuario();
+    }
+    
+  };
 }
+
+
--- how-to-vlc/src/app/services/auth.service.ts
+++ how-to-vlc/src/app/services/auth.service.ts
@@ -4,13 +4,14 @@ import { Injectable } from '@angular/core';
   providedIn: 'root'
 })
 export class AuthService {
-  isLoggedIn = true;
+  isLoggedIn = false;
   nombreUsuario: string = ''; // Inicializa nombreUsuario como una cadena vacía
+  username: string = '';
 
   private usuarios = [
     { username: 'admin', password: 'admin', nombre: 'Admin' },
     { username: 'angel', password: 'angel', nombre: 'Ángel' },
-    { username: 'triston', password: 'triston', nombre: 'Tristón'},
+    { username: 'tristan12', password: 'PlatanoManzana2024.', nombre: 'Tristán'},
     { username: 'hugo', password: 'hugo', nombre: 'Hugo' }
   ];
 
@@ -19,8 +20,10 @@ export class AuthService {
 
     if (usuario) {
       this.isLoggedIn = true; // Actualiza el estado de autenticación a true si las credenciales son correctas
-      this.nombreUsuario = usuario.nombre; // Actualiza el nombre del usuario autenticado
-      return usuario.nombre; // Devuelve el nombre del usuario autenticado
+      this.nombreUsuario = usuario.nombre; 
+      this.username = usuario.username; // Actualiza el nombre del usuario autenticado
+      return usuario.nombre;
+      return usuario?.username; // Devuelve el nombre del usuario autenticado
     } else {
       return null;
     }
@@ -33,6 +36,10 @@ export class AuthService {
 
   obtenerNombreUsuario(): string {
     return this.nombreUsuario;
+  }
+
+  getUsername(): string {
+    return this.username;
   }
 }
 
