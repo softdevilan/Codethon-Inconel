098248b69d452d460dd81255b014c7dacc112851
Ujinis <164408882+Ujinis@users.noreply.github.com>
2024-05-23 21:07:58 +0200

    Pagina Eventos

--- how-to-vlc/src/app/pages/eventos/eventos.component.ts
+++ how-to-vlc/src/app/pages/eventos/eventos.component.ts
@@ -1,11 +1,15 @@
 import { Component } from '@angular/core';
-
+import { Router } from '@angular/router';
+ 
 @Component({
   selector: 'app-eventos',
   templateUrl: './eventos.component.html',
   styleUrls: ['./eventos.component.css']
 })
 export class EventosComponent {
+  constructor(private router: Router) {}
+ 
+ 
   evento1={
     foto:'https://grupomemorable.com/wp-content/uploads/2023/06/JV_SOFOCCO_10JULIO.jpg',
     nombre:'Pirulo',
